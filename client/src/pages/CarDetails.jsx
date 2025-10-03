abcfd929884a9fd89b847af947db7a13494ae253
prasadmagdum <prasadmagdum307@gmail.com>
2025-10-03 22:29:03 +0530

    cardetailschanges

--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -1,5 +1,7 @@
 // src/pages/CarDetails.jsx
 import React, { useEffect } from 'react'
+import { assets } from '../assets/assets';
+import { dummyCarData } from '../assets/assets';
 
 const CarDetails = () => {
 
@@ -9,18 +11,22 @@ const CarDetails = () => {
    const[car,setCar]= useState(null);
 
   useEffect(() => {
-    setCar(dummyCarData.find((car) => car.id === parseInt(id)));
+    setCar(dummyCarData.find(car => car._id === id))
     scrollTo(0,0);
-  }, [id]);
+  }, [id])
+ 
+  return car ? (
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 mt-16'>
+      <button onclick={()=> navigate(-1) } className='flex items-center gap-2 text-gray-500 mb-6 cursor-pointer'>
+        
+        <img src={assets.arrow_icon} alt=" " className='rotate-180 opacity-65'/>
+        Back to all cars
+      </button>
 
-  if (!car) {
-    return <div>Loading...</div>;
-  }   
-  return (
-    <div>
-      <h1>Car Details Page</h1>
+
     </div>
-  )
+    
+  ) : <p>Loading...</p>
 }
 
 export default CarDetails
